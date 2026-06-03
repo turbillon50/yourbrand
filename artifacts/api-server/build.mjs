10362c952e8fb1797b554142f994a07195ed6313
turbillon50 <turbillon50@gmail.com>
2026-06-02 21:48:18 -0600

    feat: add lib, scripts and remaining config files

--- artifacts/api-server/build.mjs
+++ artifacts/api-server/build.mjs
@@ -25,6 +25,11 @@ async function buildAll() {
     outdir: distDir,
     outExtension: { ".js": ".mjs" },
     logLevel: "info",
+    // Some packages may not be bundleable, so we externalize them, we can add more here as needed.
+    // Some of the packages below may not be imported or installed, but we're adding them in case they are in the future.
+    // Examples of unbundleable packages:
+    // - uses native modules and loads them dynamically (e.g. sharp)
+    // - use path traversal to read files (e.g. @google-cloud/secret-manager loads sibling .proto files)
     external: [
       "*.node",
       "sharp",
@@ -101,8 +106,10 @@ async function buildAll() {
     ],
     sourcemap: "linked",
     plugins: [
+      // pino relies on workers to handle logging, instead of externalizing it we use a plugin to handle it
       esbuildPluginPino({ transports: ["pino-pretty"] })
     ],
+    // Make sure packages that are cjs only (e.g. express) but are bundled continue to work in our esm output file
     banner: {
       js: `import { createRequire as __bannerCrReq } from 'node:module';
 import __bannerPath from 'node:path';
